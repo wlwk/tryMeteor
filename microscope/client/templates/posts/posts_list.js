367f5442340289280fd5f3d6c1a4a01126ed59e2
wlwk <wangkunwanglin1988@163.com>
2016-09-18 02:56:28 -0400

    page

--- microscope/client/templates/posts/posts_list.js
+++ microscope/client/templates/posts/posts_list.js
@@ -13,8 +13,8 @@ var postsData = [
   }
 ];
 
-Template.postsList.helpers({
+/*Template.postsList.helpers({
   posts: function() {
     return Posts.find({}, {sort: {submitted: -1}});
   }
-});
+});*/
